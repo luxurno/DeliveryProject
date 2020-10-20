e84bb2f403e327b1067b3bb067e0e4acd0241584
Marcin Szostak <marcin.szostak@betsys.com>
2020-10-20 06:24:57 +0200

    Updated modules order in navbar

--- assets/js/Navbar/Navbar.js
+++ assets/js/Navbar/Navbar.js
@@ -12,32 +12,30 @@ class Navbar extends Component {
                             <Link className={"nav-link"} to={"/"}>Luxurno</Link>
                         </li>
                         <li className={"nav-item"}>
-                            <Link className={"nav-link"} to={"/import-delivery"}>Import dostaw</Link>
-                        </li>
-                        <li className={"nav-item"}>
                             <Link className={"nav-link"} to={"/settings"}>Konfiguracja</Link>
                         </li>
                         <li className={"nav-item"}>
-                            <Link className={"nav-link"} to={"/route-lookup"}>Podgląd trasy</Link>
+                            <Link className={"nav-link"} to={"/import-delivery"}>Import dostaw</Link>
                         </li>
                         <li className={"nav-item"}>
                             <Link className={"nav-link"} to={"/route-generator"}>Generator trasy</Link>
                         </li>
                         <li className={"nav-item"}>
+                            <Link className={"nav-link"} to={"/route-lookup"}>Podgląd trasy</Link>
+                        </li>
+                        <li className={"nav-item"}>
                             <Link className={"nav-link"} to={"/sending-perception"}>Wysyłanie odbioru</Link>
                         </li>
-
                         <li className="nav-item dropdown">
                             <img src={userIcon} />
                             <Link className="nav-link dropdown-toggle" to={"#"} id="navbarDropdown" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-
                             </Link>
                             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <Link className="dropdown-item" to={"/settings"}>Konfiguracja</Link>
                                 <Link className="dropdown-item" to={"/import-delivery"}>Import dostaw</Link>
-                                <Link className="dropdown-item" to={"/settings"}>Settings</Link>
-                                <Link className="dropdown-item" to={"/route-lookup"}>Podgląd trasy</Link>
                                 <Link className="dropdown-item" to={"/route-generator"}>Generator trasy</Link>
+                                <Link className="dropdown-item" to={"/route-lookup"}>Podgląd trasy</Link>
                                 <Link className="dropdown-item" to={"/sending-perception"}>Wysyłanie odbioru</Link>
                             </div>
                         </li>
