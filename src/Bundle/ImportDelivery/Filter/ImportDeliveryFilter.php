864b67f234d9ce42879533f4ade9e2136fbad136
Marcin Szostak <marcin.szostak@betsys.com>
2020-10-23 18:28:09 +0200

    Removed importing headers from import exporter

--- src/Bundle/ImportDelivery/Filter/ImportDeliveryFilter.php
+++ src/Bundle/ImportDelivery/Filter/ImportDeliveryFilter.php
@@ -6,7 +6,7 @@ namespace App\Bundle\ImportDelivery\Filter;
 
 class ImportDeliveryFilter
 {
-    public function filterHeaders(array $importData): array
+    public function filterHeaders(array &$importData): array
     {
         $headers = array_shift($importData['data'][0]);
         unset($importData['data'][0]);
