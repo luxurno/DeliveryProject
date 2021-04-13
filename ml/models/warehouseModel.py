ff7205b741e68cb24c5b3157a465f3f434fa6fbe
Luxurno Marcin Szostak <luxurno@luxurno.com>
2021-04-13 17:16:48 +0200

    Creating models step list

---
+++ ml/models/warehouseModel.py
@@ -0,0 +1,14 @@
+# Step 1 - Read warehousePackagesDB.csv
+# Step 2 - Build Test Sets
+# Step 3 - Create Filter methods datasets
+# Step 4 - Building vocabulary datasets
+# Step 5 - Save vocabulary dataset
+# Step 6 - Create encoder
+# Step 7 - Create encoder function
+# Step 8 - Create encoder mapping function
+# Step 9 - Create training model
+# Step 10 - Create testing model
+# Step 11 - Declare model
+# Step 12 - Compile, train model
+# Step 13 -
+
